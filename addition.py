8552cbd03cc096f39fafd932c658610a08bf7cb3
bhoomikamb <bhoomikambbhoomi@gmail.com>
2026-03-01 01:34:10 +0530

    Delete addition.py

--- addition.py
+++
@@ -1,8 +0,0 @@
-a=int(input("Enter a number:"))
-b=int(input("Enter another number:"))
-c=a+b
-d=a/b
-e=a-b
-print(f"addition of {a} and{b} is {c}")
-print(f"Division of {a} and{b} is {d}")
-print(f"subtration of {a} and {b} is {e}")
